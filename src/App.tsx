a030a528441e41c62c0bb88067764d74b8cfdfa1
Polina <arseneva.paulina@yandex.ru>
2026-06-21 20:49:47 +0300

    Первая версия проекта pizza-app

---
+++ src/App.tsx
@@ -0,0 +1,22 @@
+import { useState, useEffect } from 'react';
+import Button from "./components/Button/Button";
+
+function App() {
+  const [counter, setCounter] = useState<number>(0);
+
+  const addCounter = (e: MouseEvent) => {
+    console.log(e);
+  }
+
+  useEffect(() => { }, [])
+
+  return (
+    <>
+      <Button onClick={addCounter}>Кнопка</Button>
+    </>
+  )
+}
+
+export default App
+
+
---
+++ src/components/Button/Button.tsx
@@ -0,0 +1,11 @@
+import styles from './Button.module.css';
+import cn from 'classnames';
+import { ButtonProps } from './Button.props';
+
+function Button({ children, className, ...props }: ButtonProps) {
+    return (
+        <button className={cn((styles['button']), styles['accent'], className)} {...props}>{children}</button>
+    )
+}
+
+export default Button;
